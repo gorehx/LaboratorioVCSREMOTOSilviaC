d4f98babde83b09e81edb710e0190a8bec07e8d5
gorehx <47457787+gorehx@users.noreply.github.com>
2019-02-23 10:53:38 -0500

    "Solución"

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-b= int(input("Digite el número"))
-d= float(input("Digite el número"))
-cuadrado= (b*b)
-raíz=(d**(1/2))
-print("El cuadrado del número es",cuadrado,)
-print("La raíz del número es",raíz,)
+a= float(input("Digite el número"))
+b= float(input("Digite el número"))
+c= float(input("Digite el número"))
+d=(((b**2)-(4*a*c))**(1/2))
+x1=(-b+(d**(1/2)))/(2*a)
+x2=(-b-(d**(1/2)))/(2*a)
+igual= ((-1*b)/(2*a))
+if d>0:
+  print ("La parte positiva es",x1,"La parte negativa es",x2)
+else:
+  if d==0:
+   print ("El resultado es",igual)
+  else:
+   print ("No existe solución en los números reales")
+
+  
+  
+  
+  
